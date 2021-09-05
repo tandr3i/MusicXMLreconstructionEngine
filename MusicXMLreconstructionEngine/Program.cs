3a1aab06bf1c927789e2eb0d8f943b069e695c5e
Andrei Tuţu <a.tutu@dairydatawarehouse.com>
2021-09-05 12:36:30 +0300

    Add project files.

---
+++ MusicXMLreconstructionEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicXMLreconstructionEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
